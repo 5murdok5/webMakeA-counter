d2087207ba18112efeee96d06e6d72f7e0476789
5mudok5 <santiago.moreno52@gmail.com>
2020-04-22 20:56:37 -0500

    add counter animation and complete internal register, no db only local

--- js/app.js
+++ js/app.js
@@ -1,48 +1,91 @@
+/* // Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCibDxDovJlSl23xl6Lhrk_Q-69T0ozhJM",
+    authDomain: "contadores-19b0e.firebaseapp.com",
+    databaseURL: "https://contadores-19b0e.firebaseio.com",
+    projectId: "contadores-19b0e",
+    storageBucket: "contadores-19b0e.appspot.com",
+    messagingSenderId: "421095019754",
+    appId: "1:421095019754:web:ac9c20aceec50e164a3393",
+    measurementId: "G-E5SG82VJTP"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database(); */
+
 // Product Constructor
+
 class Counters {
-    constructor(count_id, count_name, count_value, count_colorback, count_colortext) {
+    constructor(count_id, count_name, count_value) {
         this.count_id = count_id;
         this.count_name = count_name;
         this.count_value = count_value;
-        this.count_colorback = count_colorback;
-        this.count_colortext = count_colortext;
     }
 }
+
 class UserData {
     constructor(user_name, user_pass) {
         this.user_name = user_name;
         this.user_pass = user_pass;
-        //this.user_email = user_email;
+       // this.user_email = user_email;
 
     }
 }
+/* 
+class dbinformation {
+
+
+    watchTable() {
+        console.log('dentro de watch');
+        const ui = new UI();
+        var refCount = firebase.ref('/users/SXYqY9NzCyXaunUJSqeOYWZqwCh1/counters/');
+        refCount.on("child_added", function (data) {
+          var countvalue = data.val();
+          const contadores = new Counters(countvalue.count_id, countvalue.count_name, countvalue.count_value);
+          var result = ui.addCounterPreview(contadores);
+          innerHTML("loadCounter", result);
+        });
+      }
+
+
+} */
+
 class LoginUser {
-    loginUser(User, Pass) {
-        if (User === "samoreno2" || Pass === "12345") {
-            window.location = '../src/count.html';
-        } else {
-            alert("vuelve a ingresar las contrasenias");
+    logUser(user, pass) {
+        var access;
+        if (user === 'admin') {
+            if (pass === '12345') {
+
+                access = true;
+            } else {
+                access = false;
+            }
         }
+        return access;
     }
 
-    registerUser(NameUser, password, email) {
-
-    }
-    isLogued(userIdActual) {
-
-    }
     validar_email(email) {
         var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         return regex.test(email) ? true : false;
     }
+
     validar_pass(pass, repass) {
         return pass === repass ? true : false;
+    }
+    /* guarda los datos para luego ser almacenados */
+    dataInforU(username, email) {
+
+        localStorage.setItem('user', username);
+        localStorage.setItem('email', email);
+        console.log("datos guardados en localstorage");
+
     }
 }
 
 // UI Constructor
 class UI {
-    addProduct(Counters) {
+
+    addCounter(Counters) {
         const userDatalist = document.getElementById('data_list');
         /* crea un elemento en el dom con el contador */
         const element = document.createElement('tr');
@@ -50,26 +93,32 @@ class UI {
             <td> ${Counters.count_name} </td>
             <td> ${Counters.count_value} </td>
             <td> <a href="#" class="btn btn-danger" name="delete">Delete</a> </td>
-            <td> <a href="#" class="btn btn-warning" name="edit">Editar</a> </td>
-        `;
+            <td> <a href="#" class="btn btn-warning" name="edit">Editar</a> </td>`;
         userDatalist.appendChild(element);
     }
-    
-    addProductcounter(Counters) {
+
+    addCounterPreview(Counters) {
         const userDatalist = document.getElementById('counters_row');
         /* crea un elemento en el dom con el contador */
         const element = document.createElement('div');
         element.className = 'col-sm';
         element.name = 'delete'
         element.innerHTML = `
-            <h1>
+            <h1 class="num" id="count_num">
                 ${Counters.count_value} 
             </h1>
-            <h5>
+            <h5 id="count_title">
                 ${Counters.count_name} 
-            </h5>
-        `;
+            </h5>`;
         userDatalist.appendChild(element);
+        this.startAnimation();
+    }
+
+    startAnimation() {
+        $(".num").counterUp({
+            delay: 5,
+            time: 3000
+        });
     }
 
     hidenObj(elemnt, status) {
@@ -80,13 +129,13 @@ class UI {
         document.getElementById(formid).reset();
     }
 
-    deleteProduct(element) {
+    deleteCounters(element) {
         if (element.name === 'delete') {
             element.parentElement.parentElement.remove();
             this.showMessage('Product Deleted Succsssfully', 'info');
         }
     }
-
+    /* control de mensajes de alerta */
     showMessage(message, cssClass) {
         const div = document.createElement('div');
         div.className = `container alert alert-${cssClass} mt-2 fixed-top`;
@@ -103,44 +152,51 @@ class UI {
         $(".alert").fadeOut(4000);
 
     }
+
 }
 
-
-
-
 document.getElementById("inicio").addEventListener("click", function (e) {
+
     const ui = new UI();
     const lg = new LoginUser();
     const user = document.getElementById('NameUser').value,
         pass = document.getElementById('PassWord').value;
 
-    if (user === 'admin' || pass === '12345') {
+    var access = lg.logUser(user, pass);
 
+    if (access === true) {
         ui.hidenObj('lgform', 'none');
         ui.hidenObj('homelg', '');
+
         ui.showMessage('ingreso exitoso', 'success');
+
     } else {
         ui.showMessage('Nombre de Usuario o Password Invalidas', 'danger');
     }
-
+    
     e.preventDefault();
 });
 
 
 
 /* funcion disparadora registro */
+
 document.getElementById("registrate").addEventListener("click", function (e) {
+
     const ui = new UI();
     const lg = new LoginUser();
+
     ui.hidenObj('inicio', 'none');
     ui.hidenObj('re-pass', '');
     ui.hidenObj('correo', '');
 
     const user = document.getElementById('NameUser').value,
         pass = document.getElementById('PassWord').value,
-        email = document.getElementById('email').value,
+       // email = document.getElementById('email').value,
         repass = document.getElementById('re-PassWord').value;
+
     const newUser = new UserData(user, pass);
+
     if (email === '' || repass === '') {
         ui.showMessage('rellene los campos de registro', 'info');
 
@@ -155,7 +211,7 @@ document.getElementById("registrate").addEventListener("click", function (e) {
             ui.hidenObj('correo', 'none');
             ui.resetForm();
             ui.showMessage('Product Added Successfully', 'success');
-            ui.addProduct(newUser);
+            ui.addCounter(newUser);
         } else {
             ui.showMessage('El correo esta mal', 'danger');
         }
@@ -165,7 +221,7 @@ document.getElementById("registrate").addEventListener("click", function (e) {
     e.preventDefault();
 });
 
-
+/* agrega un contador */
 document.getElementById('counter_form')
     .addEventListener('submit', function (e) {
 
@@ -173,7 +229,7 @@ document.getElementById('counter_form')
             value = document.getElementById('valueCounter').value;
 
         // Create a new Oject Product
-        const product = new Counters("1", name, value, "#ccc", "#ccc");
+        const counter = new Counters('1', name, value);
 
         // Create a new UI
         const ui = new UI();
@@ -182,22 +238,20 @@ document.getElementById('counter_form')
         if (name === '' || value === '') {
             ui.showMessage('Please Insert data in all fields', 'danger');
         } else {
-
             // Save Product
-            ui.addProduct(product);
-            ui.addProductcounter(product);
-            console.log(product)
+            ui.addCounter(counter);
+            ui.addCounterPreview(counter);
             ui.showMessage('counter Added Successfully', 'success');
             ui.resetForm('counter_form');
+            console.log(counter);
         }
         e.preventDefault();
 
     });
-
+/* boton para eliminar los elementos de las celdas*/
 document.getElementById('counters_list')
     .addEventListener('click', function (e) {
         const ui = new UI();
-        ui.deleteProduct(e.target);
-        ui.deleteProduct('div');
+        ui.deleteCounters(e.target);
         e.preventDefault();
     });
